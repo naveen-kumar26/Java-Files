69a22c34192a7e6cc928095999ac3a67edde4ac3
naveen-kumar26 <119224926+naveen-kumar26@users.noreply.github.com>
2023-11-17 12:08:50 +0530

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -1,1 +1,41 @@
-
+import java.util.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String str1 = s.nextLine();
+        String str2 = s.nextLine();
+        int count=0;
+        
+        if(str1.length()==str2.length())
+        {
+            for (int i=0;i<str1.length();i++ )
+            {
+                for(int j=0;j<str2.length();j++)
+                {
+                    if(str1.charAt(i)!=str2.charAt(j))
+                    {
+                        continue;
+                    }
+                    else if(str1.charAt(i)==str2.charAt(j))
+                    {
+                        count+=1;
+                        break;
+                    }
+                }
+            }
+        }
+        //System.out.println(count);
+        if(count==str1.length())
+        {
+            System.out.println("yes");
+        }
+        else if( (count!=str1.length()) || (str1.length() != str2.length()) )
+        {
+            System.out.println("no");
+        }
+        
+        
+    }
+}
